4c61e912525602eb1c788b875fb175a18ccf5537
lukas <lambry@wedigital.garden>
2024-10-05 11:00:19 +0200

    fixtures and migrations

--- migrations/Version20241004122517.php
+++ migrations/Version20241004122517.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20241004122517 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,26 +16,36 @@ final class Version20241004122517 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, label VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE comment (id INT AUTO_INCREMENT NOT NULL, content LONGTEXT NOT NULL, status VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE episode (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, duration INT NOT NULL, released_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE language (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, code VARCHAR(3) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE media (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE playlist (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE playlist_media (id INT AUTO_INCREMENT NOT NULL, added_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE playlist_subscription (id INT AUTO_INCREMENT NOT NULL, subscribed_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE season (id INT AUTO_INCREMENT NOT NULL, number VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE subscription (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, duration INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE subscription_history (id INT AUTO_INCREMENT NOT NULL, start_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', end_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE `user` (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, account_status VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE watch_history (id INT AUTO_INCREMENT NOT NULL, last_watched_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', number_of_views INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE category (id SERIAL NOT NULL, nom VARCHAR(255) NOT NULL, label VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE comment (id SERIAL NOT NULL, content TEXT NOT NULL, status VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE episode (id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, duration INT NOT NULL, released_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE language (id SERIAL NOT NULL, nom VARCHAR(255) NOT NULL, code VARCHAR(3) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE media (id SERIAL NOT NULL, type VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE playlist (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE playlist_media (id SERIAL NOT NULL, added_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE playlist_subscription (id SERIAL NOT NULL, subscribed_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE subscription (id SERIAL NOT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, duration INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE subscription_history (id SERIAL NOT NULL, start_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, end_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE "user" (id SERIAL NOT NULL, username VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, account_status VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE watch_history (id SERIAL NOT NULL, last_watched_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, number_of_views INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGSERIAL NOT NULL, body TEXT NOT NULL, headers TEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, available_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, delivered_at TIMESTAMP WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE serie (id INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE movie (id INT NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE season (id SERIAL NOT NULL, number VARCHAR(255) NOT NULL, serie_id INT DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('ALTER TABLE serie ADD CONSTRAINT FK_EE5755D5BF396750 FOREIGN KEY (id) REFERENCES media (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE movie ADD CONSTRAINT FK_1F9E6D2CBF396750 FOREIGN KEY (id) REFERENCES media (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE season ADD CONSTRAINT FK_F0E45BA42F7B8B3 FOREIGN KEY (serie_id) REFERENCES serie (id) ON DELETE CASCADE');
+        $this->addSql('CREATE INDEX IDX_75EA56E0FB7336F0 ON messenger_messages (queue_name)');
+        $this->addSql('CREATE INDEX IDX_75EA56E0E3BD61CE ON messenger_messages (available_at)');
+        $this->addSql('CREATE INDEX IDX_75EA56E016BA31DB ON messenger_messages (delivered_at)');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE season DROP CONSTRAINT FK_F0E45BA42F7B8B3');
+        $this->addSql('ALTER TABLE serie DROP CONSTRAINT FK_EE5755D5BF396750');
+        $this->addSql('ALTER TABLE movie DROP CONSTRAINT FK_1F9E6D2CBF396750');
+
         $this->addSql('DROP TABLE category');
         $this->addSql('DROP TABLE comment');
         $this->addSql('DROP TABLE episode');
@@ -50,8 +57,9 @@ final class Version20241004122517 extends AbstractMigration
         $this->addSql('DROP TABLE season');
         $this->addSql('DROP TABLE subscription');
         $this->addSql('DROP TABLE subscription_history');
-        $this->addSql('DROP TABLE `user`');
+        $this->addSql('DROP TABLE "user"');
         $this->addSql('DROP TABLE watch_history');
         $this->addSql('DROP TABLE messenger_messages');
+        $this->addSql('DROP TABLE serie');
     }
 }
--- migrations/Version20241004133045.php
+++ migrations/Version20241004133045.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20241004133045 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,23 +16,33 @@ final class Version20241004133045 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE media_category (media_id INT NOT NULL, category_id INT NOT NULL, INDEX IDX_92D3773EA9FDD75 (media_id), INDEX IDX_92D377312469DE2 (category_id), PRIMARY KEY(media_id, category_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE media_media (media_source INT NOT NULL, media_target INT NOT NULL, INDEX IDX_753565BDE3F3E5AD (media_source), INDEX IDX_753565BDFA16B522 (media_target), PRIMARY KEY(media_source, media_target)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE media_category (media_id INT NOT NULL, category_id INT NOT NULL, PRIMARY KEY(media_id, category_id))');
+        $this->addSql('CREATE INDEX IDX_92D3773EA9FDD75 ON media_category (media_id)');
+        $this->addSql('CREATE INDEX IDX_92D377312469DE2 ON media_category (category_id)');
+        $this->addSql('CREATE TABLE media_media (media_source INT NOT NULL, media_target INT NOT NULL, PRIMARY KEY(media_source, media_target))');
+        $this->addSql('CREATE INDEX IDX_753565BDE3F3E5AD ON media_media (media_source)');
+        $this->addSql('CREATE INDEX IDX_753565BDFA16B522 ON media_media (media_target)');
         $this->addSql('ALTER TABLE media_category ADD CONSTRAINT FK_92D3773EA9FDD75 FOREIGN KEY (media_id) REFERENCES media (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE media_category ADD CONSTRAINT FK_92D377312469DE2 FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE media_media ADD CONSTRAINT FK_753565BDE3F3E5AD FOREIGN KEY (media_source) REFERENCES media (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE media_media ADD CONSTRAINT FK_753565BDFA16B522 FOREIGN KEY (media_target) REFERENCES media (id) ON DELETE CASCADE');
+        $this->addSql('CREATE TABLE media_language (media_id INT NOT NULL, language_id INT NOT NULL, PRIMARY KEY(media_id, language_id))');
+        $this->addSql('CREATE INDEX IDX_MEDIA_LANGUAGE_MEDIA_ID ON media_language (media_id)');
+        $this->addSql('CREATE INDEX IDX_MEDIA_LANGUAGE_LANGUAGE_ID ON media_language (language_id)');
+        $this->addSql('ALTER TABLE media_language ADD CONSTRAINT FK_MEDIA_LANGUAGE_MEDIA_ID FOREIGN KEY (media_id) REFERENCES media (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE media_language ADD CONSTRAINT FK_MEDIA_LANGUAGE_LANGUAGE_ID FOREIGN KEY (language_id) REFERENCES language (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE media_category DROP FOREIGN KEY FK_92D3773EA9FDD75');
-        $this->addSql('ALTER TABLE media_category DROP FOREIGN KEY FK_92D377312469DE2');
-        $this->addSql('ALTER TABLE media_media DROP FOREIGN KEY FK_753565BDE3F3E5AD');
-        $this->addSql('ALTER TABLE media_media DROP FOREIGN KEY FK_753565BDFA16B522');
+        $this->addSql('ALTER TABLE media_category DROP CONSTRAINT FK_92D3773EA9FDD75');
+        $this->addSql('ALTER TABLE media_category DROP CONSTRAINT FK_92D377312469DE2');
+        $this->addSql('ALTER TABLE media_media DROP CONSTRAINT FK_753565BDE3F3E5AD');
+        $this->addSql('ALTER TABLE media_media DROP CONSTRAINT FK_753565BDFA16B522');
+        $this->addSql('ALTER TABLE media_language DROP CONSTRAINT FK_MEDIA_LANGUAGE_MEDIA_ID');
+        $this->addSql('ALTER TABLE media_language DROP CONSTRAINT FK_MEDIA_LANGUAGE_LANGUAGE_ID');
         $this->addSql('DROP TABLE media_category');
         $this->addSql('DROP TABLE media_media');
+        $this->addSql('DROP TABLE media_language');
     }
 }
